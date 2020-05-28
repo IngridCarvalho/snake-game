80b04831a23b8888f9af2e3bf05d7a60cb626a91
Ingrid Carvalho <ingridcarvalho_1007@hotmail.com>
2020-05-28 09:31:03 -0300

    Done the snake game and some changes in the layout

---
+++ js/script.js
@@ -0,0 +1,104 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+let direction = "right";
+
+// initial random food position
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+// initial snake position
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+function createBackground() {
+    context.fillStyle = "aquamarine";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function createSnake() {
+    for(i=0; i<snake.length; i++){
+        context.fillStyle = "teal";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+//takes the keydown events
+document.addEventListener("keydown", update);
+
+//verify positions
+function update(event) {
+    if(event.keyCode == 37 && direction != "right") {
+        direction = "left";
+    }
+
+    if(event.keyCode == 38 && direction != "down") {
+        direction = "up";
+    }
+
+    if(event.keyCode == 39 && direction != "left") {
+        direction = "right";
+    }
+
+    if(event.keyCode == 40 && direction != "up") {
+        direction = "down";
+    }
+}
+
+function startGame() {
+
+    // makes the snake return in the opposite position
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    // if the snake collides with the body, game over
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game);
+            alert('Game over!');
+        }
+    }
+
+    createBackground();
+    createSnake();
+    drawFood(); 
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    
+    // checks directions and makes the snake walk
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    // if the snake is in the same position as the food, then it appears in another position
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y =  Math.floor(Math.random() * 15 + 1) * box;
+    }
+
+    // generates the next head of the snake while it is removed
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
